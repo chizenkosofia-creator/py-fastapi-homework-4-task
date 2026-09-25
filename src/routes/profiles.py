1bd0b9ca8d643e8e5d731e40710fd2afb8cc0583
Sofia <chizenko.sofia@gmail.com>
2026-09-25 14:57:10 +0300

    solution13

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -120,7 +120,7 @@ async def create_profile(
         validate_image(avatar)
     except ValueError as e:
         raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
         )
 
